8730135cabcfcef59ec4d80c750eac7b386e1e4d
Yuta Arashita <yuta.arashita@di-v.co.jp>
2022-05-28 23:47:24 +0900

    add: タスク詳細コンポーネント実装

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 const routes = [
   {
     path: '/tasks',
     name: 'task.list',
     component: TaskListComponent
+  },
+  {
+    path: '/tasks/:taskId',
+    name: 'task.show',
+    component: TaskShowComponent,
+    props: true
   },
 ];
 
