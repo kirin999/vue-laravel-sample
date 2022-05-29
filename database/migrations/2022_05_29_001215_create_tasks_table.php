002817eb3396c947751b90630f0638d3a98e8222
Yuta Arashita <yuta.arashita@di-v.co.jp>
2022-05-29 09:23:34 +0900

    update: 微修正

--- database/migrations/2022_05_29_001215_create_tasks_table.php
+++ database/migrations/2022_05_29_001215_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->bigIncrements();
+            $table->id();
             $table->string('title', 100);
             $table->string('content', 100);
             $table->string('person_in_charge', 100);
