6075447506b647862a1bf11903df90d5665a378c
Yuta Arashita <yuta.arashita@di-v.co.jp>
2022-05-30 08:31:19 +0900

    update: タスク詳細ページAPI繋ぎ込み

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -7,28 +7,28 @@
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
                         <div class="col-sm-9">
                             <input type="text" class="form-control-plaintext" readonly id="id"
-                                   v-bind:value="taskId">
+                                   v-model="task.id">
                         </div>
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
                         <div class="col-sm-9">
                             <input type="text" class="form-control-plaintext" readonly id="title"
-                                   value="title title">
+                                   v-model="task.title">
                         </div>
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
                         <div class="col-sm-9">
                             <input type="text" class="form-control-plaintext" readonly id="content"
-                                   value="content content">
+                                   v-model="task.content">
                         </div>
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
                         <div class="col-sm-9">
                             <input type="text" class="form-control-plaintext" readonly id="person-in-charge"
-                                   value="Ichiro">
+                                   v-model="task.person_in_charge">
                         </div>
                     </div>
                 </form>
@@ -38,9 +38,27 @@
 </template>
 
 <script>
+import axios from 'axios'
+
     export default {
         props: {
             taskId: String
+        },
+        data: function () {
+            return {
+                task: {}
+            }
+        },
+        methods: {
+            getTask() {
+                axios.get('/api/tasks/' + this.taskId)
+                    .then((res) => {
+                        this.task = res.data;
+                    });
+            }
+        },
+        mounted() {
+            this.getTask();
         }
     }
 </script>
