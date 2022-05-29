bfd7f43401f7f29988e6db99b64d1e6feccd2953
Yuta Arashita <yuta.arashita@di-v.co.jp>
2022-05-29 09:33:51 +0900

    add: タスク登録API実装

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('tasks', [TaskController::class, 'store']);
 Route::get('/tasks/{task}', [TaskController::class, 'show']);
